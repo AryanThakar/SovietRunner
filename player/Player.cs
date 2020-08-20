e5f990295bde8bfc6e2f64c9a111ca00c2a3586b
Qapples <armand.scotty@yahoo.com>
2020-08-20 17:02:13 -0400

    Fix duplicate player.cs in .sln and fix formatting in player.cs

--- player/Player.cs
+++ player/Player.cs
@@ -1,30 +1,30 @@
 using Godot;
 using System;
 
-public class Player : KinematicBody2D {
-    [Export]
+public class Player : KinematicBody2D
+{
+    [Export] public int Speed = 250;
 
-    public int speed = 250;
-
-    public override void _PhysicsProcess(float delta) {
-
+    public override void _PhysicsProcess(float delta)
+    {
         var motion = new Vector2();
-        
+
         Vector2 textureSize = GetNode<Texture>("Sprite").GetSize();
         Vector2 screenSize = GetViewport().GetVisibleRect().Size;
-               
+
         motion.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
         motion.y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
-        
-        if (Position.x + motion.x < 0 || Position.x + motion.x + textureSize.x > screenSize.x) {
+
+        if (Position.x + motion.x < 0 || Position.x + motion.x + textureSize.x > screenSize.x)
+        {
             motion.x = 0;
-        } if (Position.y + motion.y < 0 || Position.y + motion.y + textureSize.y > screenSize.y) {
+        }
+
+        if (Position.y + motion.y < 0 || Position.y + motion.y + textureSize.y > screenSize.y)
+        {
             motion.y = 0;
-        } 
-        
-        MoveAndCollide(motion.Normalized() * speed * delta);
+        }
 
-        
+        MoveAndCollide(motion.Normalized() * Speed * delta);
     }
-
 }
